50a340a72648e6b2723cd1c329411650046fdc75
vinsean <44472520+vinsean@users.noreply.github.com>
2023-05-09 17:53:05 +0800

    Update zhitiao2.js

--- JS/zhitiao2.js
+++ JS/zhitiao2.js
@@ -4,5 +4,8 @@ zhitiao app unlocks pro
 QX
 ^https:\/\/app1\.zuowenzhitiao\.com\/user\/member\/details url script-response-body https://raw.githubusercontent.com/vinsean/DOCMENT/edit/master/JS/zhitiao.2js
 
-Surge & QX Mitm = api.picsart.c*, api.meiease.c*
+Surge & QX Mitm = app1.zuowenzhitiao.com
 */
+let obj = JSON.parse($response.body);
+obj.data = {"status":"1", "endTime":"1694188800000", "userId":"65795493", "type":"1"};
+$done({body: JSON.stringify(obj)});
