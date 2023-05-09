12a8d2d62301d94a9697ecc7b68b76027cd8568e
vinsean <44472520+vinsean@users.noreply.github.com>
2023-05-09 17:51:20 +0800

    Create zhitiao2.js

---
+++ JS/zhitiao2.js
@@ -0,0 +1,8 @@
+/*
+zhitiao app unlocks pro
+
+QX
+^https:\/\/app1\.zuowenzhitiao\.com\/user\/member\/details url script-response-body https://raw.githubusercontent.com/vinsean/DOCMENT/edit/master/JS/zhitiao.2js
+
+Surge & QX Mitm = api.picsart.c*, api.meiease.c*
+*/
