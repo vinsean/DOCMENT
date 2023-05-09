3a5f538d6a6e57dc326d3eecbca5682260163ef1
vinsean <44472520+vinsean@users.noreply.github.com>
2023-05-09 18:26:36 +0800

    Update zhitiao1.js

--- JS/zhitiao1.js
+++ JS/zhitiao1.js
@@ -2,8 +2,8 @@
 zhitiao app unlocks pro
 
 QX
-^https:\/\/app1\.zuowenzhitiao\.com\/user\/common\/getPersonInfo url script-response-body https://raw.githubusercontent.com/vinsean/DOCMENT/edit/master/JS/zhitiao1.js
-^https:\/\/app1\.zuowenzhitiao\.com\/user\/extradata?pushSystemPermission=0 url script-response-body https://raw.githubusercontent.com/vinsean/DOCMENT/edit/master/JS/zhitiao1.js
+^https:\/\/app1\.zuowenzhitiao\.com\/user\/common\/getPersonInfo url script-response-body https://raw.githubusercontent.com/vinsean/DOCMENT/master/JS/zhitiao1.js
+^https:\/\/app1\.zuowenzhitiao\.com\/user\/extradata?pushSystemPermission=0 url script-response-body https://raw.githubusercontent.com/vinsean/DOCMENT/master/JS/zhitiao1.js
 
 Surge & QX Mitm = app1.zuowenzhitiao.com
 */
@@ -13,10 +13,10 @@ let obj = JSON.parse($response.body);
 /*
 var s1 = '2023-09-09';
 s1 = new Date(s1.replace(/-/g, "/"));
-s2 = new Date();//当前日期
+s2 = new Date();
 var days = s1.getTime() - s2.getTime();
 var experienceDays = parseInt(days / (1000 * 60 * 60 * 24));
 */
-
+console.log(obj.data.member)
 obj.data.member = {"status":"1", "endTime":"1694188800000", "userId":"65795493", "type":"1", "experienceDays":"123"};
 $done({body: JSON.stringify(obj)});
